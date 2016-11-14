3d39c59a079bcb7204403648287460d00b98d1b5
sniffer <sergstwnsn@yandex.ru>
2016-11-14 23:50:03 +0300

    Добавил стилизацию для lynx-block

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,14 +6,33 @@ import {ModuleWithProviders} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {RootComponent} from "./root/root.component";
 import {MainComponent} from "./main/main.component";
+import {ItemsComponent} from "./items/items.component";
 
 const appRoutes: Routes = [
-  {path: 'app', component: AppComponent},
   {
-    path: '', component: RootComponent, children: [
-    {path: '', component: MainComponent}
-  ]
+    path: 'app',
+    component: AppComponent
   },
+  {
+    path: '',
+    component: RootComponent,
+    children: [
+      {
+        path: '',
+        component: MainComponent
+      }
+    ]
+  },
+  {
+    path: 'items',
+    component: RootComponent,
+    children: [
+      {
+        path: '',
+        component: ItemsComponent
+      }
+    ]
+  }
 ];
 
 export const appRoutingProviders: any[] = [];
