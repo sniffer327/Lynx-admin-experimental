a04262409f652aaf7f6a3aa9040077882abc7339
gromanev <gromanev@gmail.com>
2016-11-22 22:32:41 +0300

    Добавил данные в модель и сделал маршрутизацию ебаного входа

--- src/app/Models/user-info-model.ts
+++ src/app/Models/user-info-model.ts
@@ -1,2 +1,15 @@
 export class UserInfoModel {
+  public id: number;
+  public email: string;
+  public name: string;
+  public login: string;
+  public role: string;
+  public photoUrl: string;
+  public phone: string;
+  public company: string;
+  public profileBgUrl: string;
+  public subUser?: number;
+  public userHash: string;
+  public dateActivation: Date;
+  public password: string;
 }
