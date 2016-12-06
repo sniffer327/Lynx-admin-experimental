383c9bdd85e544b35e2c17c360450ca6e866cff7
sniffer <sergstwnsn@yandex.ru>
2016-12-06 22:16:18 +0300

    Добавил компонент для изображений

---
+++ src/app/custom-components/item-images/item-images.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'item-images',
+  templateUrl: './item-images.component.html',
+  styleUrls: ['./item-images.component.scss']
+})
+export class ItemImagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
