d30eb374b908f4c92ac098d6b08a3299a950ce7d
sniffer <sergstwnsn@yandex.ru>
2016-12-19 20:12:09 +0300

    Перенёс стили для lynx-table; Добавил RouterModule в зависимость; Добавил @Input списка с товарами

--- src/app/custom-components/lynx-table/lynx-table/lynx-table.component.ts
+++ src/app/custom-components/lynx-table/lynx-table/lynx-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'lynx-table',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lynx-table.component.scss']
 })
 export class LynxTableComponent implements OnInit {
+
+  @Input() items: any;
 
   constructor() { }
 
