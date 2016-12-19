cf91a28d570ebfbbf26f9bdbffcc9f930505323c
sniffer <sergstwnsn@yandex.ru>
2016-12-19 19:59:28 +0300

    Заменил шаблон для app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {AuthService} from "./Services/auth.service";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  template: `<router-outlet></router-outlet>`
 })
 
 export class AppComponent implements OnInit {
