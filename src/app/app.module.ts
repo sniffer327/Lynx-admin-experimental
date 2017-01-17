766bdf39bae73ddd93b24f2b1efab4fcdb2a78fc
sniffer <sergstwnsn@yandex.ru>
2017-01-17 22:10:56 +0300

    Обновил разметку в LayoutComponent; Обновил разметку в MainComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,6 @@ import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {routing, appRoutingProviders} from "./app.routing";
 import {MainComponent} from './main/main.component';
-import {MdSidenavModule} from "@angular2-material/sidenav";
-import {MdCoreModule} from "@angular2-material/core";
-import {MdButtonModule} from "@angular2-material/button";
-import {MdCardModule} from "@angular2-material/card";
-import {MdToolbarModule} from "@angular2-material/toolbar";
-import {MdIconModule} from "@angular2-material/icon";
 import {ItemsComponent} from './items/items.component';
 import {ItemsEditComponent} from './items-edit/items-edit.component';
 import {AuthorizationComponent} from './authorization/authorization.component';
@@ -19,7 +13,6 @@ import {AuthService} from "./Services/auth.service";
 import {LynxService} from "./Services/lynx.service";
 import {CKEditorModule} from "ng2-ckeditor";
 import {ItemImagesComponent} from './custom-components/item-images/item-images.component';
-import {MdCheckboxModule} from "@angular2-material/checkbox";
 import {CookieService} from "angular2-cookie/services/cookies.service";
 import {CanActivateService} from "./Services/can-activate.service";
 import {LynxLoggingService} from "./Services/lynx-logging.service";
@@ -43,13 +36,6 @@ import { LynxModule } from './custom-components/lynx-table/lynx-table.module';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MdCoreModule,
-    MdSidenavModule,
-    MdToolbarModule,
-    MdButtonModule,
-    MdIconModule,
-    MdCardModule,
-    MdCheckboxModule,
     CKEditorModule,
     routing,
     LynxModule
