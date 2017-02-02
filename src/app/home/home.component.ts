a26bce8bc8f778d821d8ae7e73ef5fe664de6650
sniffer <sergstwnsn@yandex.ru>
2017-02-02 19:57:02 +0300

    Добавил AuthorizationModule; Обновил HomeModule

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public modules;
+
+  constructor() {}
 
   ngOnInit() {
-  }
 
+    this.modules = [
+      {
+        title: 'Товары',
+        url: '/items',
+        img: '/assets/images/items.jpg',
+        description: 'В данном модуле Вы можете отредактировать товары'
+      },
+      {
+        title: 'Страницы',
+        url: '/pages',
+        img: '/assets/images/pages.jpg',
+        description: 'В данном модуле Вы можете отредактировать страницы'
+      },
+      {
+        title: 'Новости',
+        url: '/news',
+        img: '/assets/images/news.jpg',
+        description: 'В данном модуле Вы можете отредактировать новости'
+      }
+    ];
+  }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule }  from '@angular/common';
 
 import {HomeComponent} from "./home.component";
 import {HomeRoutingModule} from "./home-routing.module";
+import {MaterialModule} from "@angular/material";
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MaterialModule
   ],
   declarations: [
     HomeComponent
