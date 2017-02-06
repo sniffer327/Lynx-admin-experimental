f5fab1a4aca6d1f5ece636b80daa95155306fc90
sniffer <sergstwnsn@yandex.ru>
2017-02-06 20:06:56 +0300

    Дбавил компонент для редактирования item

--- src/app/items/items.module.ts
+++ src/app/items/items.module.ts
@@ -5,16 +5,26 @@ import {MaterialModule} from "@angular/material";
 import {ItemsComponent} from "./items.component";
 import {ItemsRoutingModule} from "./items-routing.module";
 import {LynxTableModule} from "../Shared/lynx-table/lynx-table.module";
+import {ItemsEditComponent} from "./items-edit.component";
+import {FormsModule} from "@angular/forms";
+import {CKEditorModule} from "ng2-ckeditor";
+import {ItemImagesComponent} from "../Shared/item-images/item-images.component";
+import {DragulaModule} from "ng2-dragula";
 
 @NgModule({
   imports: [
     CommonModule,
     ItemsRoutingModule,
+    FormsModule,
     MaterialModule,
-    LynxTableModule
+    LynxTableModule,
+    CKEditorModule,
+    DragulaModule
   ],
   declarations: [
-    ItemsComponent
+    ItemsComponent,
+    ItemsEditComponent,
+    ItemImagesComponent
   ]
 })
 export class ItemsModule {}
